a86ac8162ea7748f8b32f87642edd11111b68b38
maxwen <max.weninger@gmail.com>
2013-10-05 13:15:36 +0200

    base: add support for adb over network [1/2]
    Change-Id: If8354c92a03da501c683638cc69097d287d74816

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7609,27 +7609,10 @@ public final class Settings {
         public static final String NIGHT_BRIGHTNESS_VALUE = "night_brightness_value";
 
         /**
-         * Disable power menu on secure lock screens
-         *
+         * The TCP/IP port to run ADB on, or -1 for USB
          * @hide
          */
-        public static final String LOCK_POWER_MENU_DISABLED = "lockscreen_power_menu_disabled";
-
-	/**
-         * Force authorize Substratum (or equivalent) frontend calling packages by ThemeInterfacer
-         * The value is boolean (1 or 0).
-         * @hide
-         */
-        public static final String FORCE_AUTHORIZE_SUBSTRATUM_PACKAGES = "force_authorize_substratum_packages";
-
-        /**
-         * 0 - Automatic (Wallpaper based)
-         * 1 - Light
-         * 2 - Dark
-         * 3 - Black
-         * @hide
-         */
-        public static final String DEVICE_THEME = "device_theme";
+        public static final String ADB_PORT = "adb_port";
 
         /**
          * This are the settings to be backed up.
